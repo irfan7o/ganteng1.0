85f78724e1483e831d1cd62a5b46bd0ecaf25eb4
Irfan <kartolo727@gmail.com>
2024-06-24 11:44:01 +0800

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
 const App = () => {
+
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
+    <div className={darkTheme ? 'dark' : ''}>
       <h1>App</h1>
     </div>
   );
