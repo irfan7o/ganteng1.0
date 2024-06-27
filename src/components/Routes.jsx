96cd0bea1b18263a14429eabbc6fedf8410b2f71
Irfan <kartolo727@gmail.com>
2024-06-27 19:33:25 +0800

    Search Engine

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Routes as RouterRoutes, Route, Navigate } from 'react-router-dom';
+
+import { Results } from './Results';
 
 export const Routes = () => {
   return (
-    <div>
-      <h1>Routes</h1>
+    <div className="p-4">
+      <RouterRoutes>
+        <Route path="/" element={<Navigate to="/search" />} />
+        <Route path="/search" element={<Results />} />
+        <Route path="/images" element={<Results />} />
+        <Route path="/news" element={<Results />} />
+        <Route path="/videos" element={<Results />} />
+      </RouterRoutes>
     </div>
   );
 }
+
